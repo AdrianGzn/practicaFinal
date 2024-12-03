e326e527ef6430424049aa1287cc605c8720e6ee
AdrianGzn <233318@ids.upchiapas.edu.mx>
2024-12-03 11:01:08 -0600

    Login

--- src/app/shared/components/login/login.component.ts
+++ src/app/shared/components/login/login.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { User } from '../../models/user';
+import { FormBuilder, ReactiveFormsModule, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private router: Router) {
+    this.loginForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit(): void {
+    let name = this.loginForm.value.name;
+    let password = this.loginForm.value.password;
+
+    if (this.loginForm.valid) {
+      if (name != null && password != null) {
+        let newUser: User = {
+          name: name,
+          password: password,
+          pokemon_id: 0
+        }
+        localStorage.setItem("user", JSON.stringify(newUser));
+        this.router.navigate(['/Home']);
+      } else {
+        alert('Llene el formulario correctamente.')
+      }
+    } else {
+      alert('Llene el formulario correctamente.')
+    }
+  }
 
 }
