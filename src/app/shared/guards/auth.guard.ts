dec5558dfea89fb93de35e176caa48f6ffd981f8
Regiber18 <regiber.montoya2005@hotmail.com>
2024-12-03 10:24:41 -0600

    add home and login component

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -1,5 +1,13 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const authGuard: CanActivateFn = () => {
+  const loginUser = localStorage.getItem("user");
+  const router = inject(Router);
+  if(!loginUser) {
+    router.navigate(["./"])
+    return false; 
+  }
+
   return true;
 };
